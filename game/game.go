fc3a296cab92de4b8acf902ed55956b444c9cf1d
mick-roper <mick@theropers.co.uk>
2020-02-10 15:25:50 +0000

    added game looop

--- game/game.go
+++ game/game.go
@@ -1,5 +1,13 @@
 package game
 
+type gameState int
+
+const (
+	stateInitialising gameState = iota
+	stateRunning
+	stateExiting
+)
+
 // Logger that writes logs
 type Logger func(string)
 
@@ -8,7 +16,20 @@ type RunOptions struct {
 	Logger Logger
 }
 
+var state gameState = stateInitialising
+
 // Run the game
 func Run(opts *RunOptions) {
+	opts.Logger("initialising the game")
+	defer cleanup()
+
+	for state != stateExiting {
+		state = stateExiting
+	}
+
+	opts.Logger("exiting...")
+}
+
+func cleanup() {
 
 }
